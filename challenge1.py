3a4d1e277f8dc254d4e708485c36ec5634169796
Mohammed Rubbani <mohrubbani@hotmail.co.uk>
2021-12-21 08:09:30 +0000

    Added method for reading .txt file into Python list

--- challenge1.py
+++ challenge1.py
@@ -1,5 +1,12 @@
+def read_file(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
+
+
 def main():
-    print("Hello World!")
+    file = read_file("AoC_day1Input.txt")
+    print(file)
+
 
 if __name__ == '__main__':
     main()
